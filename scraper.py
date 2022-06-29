674e3f9c7772d5ab2f55aa610bbe5d2db7ed2750
Truman Gao <mr.trumangao@gmail.com>
2022-06-29 02:57:10 -0700

    working version to scrape requirements in uvic program

--- scraper.py
+++ scraper.py
@@ -3,25 +3,77 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
 from bs4 import BeautifulSoup
-browser = webdriver.Chrome(executable_path=r"C:\Users\Truman\Desktop\code for fun\UVic-pre-reqs-scraper\chromedriver.exe")
+import copy
+import json
+
+
+#object of Options class, passing headless parameter
+c = Options()
+c.add_argument('--headless')
+s = Service('chromedriver v103.exe')
+browser = webdriver.Chrome(service=s, options=c)
 browser.set_window_size(1120, 550)
-browser.get('https://www.uvic.ca/calendar/undergrad/index.php#/programs/S1gtLTm0ME')
 
-## gets pre reqs container
-# element = WebDriverWait(browser, 5).until(
-#    EC.presence_of_element_located((By.CLASS_NAME, "rules-wrapper"))
-# )
+program_url = 'https://www.uvic.ca/calendar/undergrad/index.php#/programs/S1gtLTm0ME'
+browser.get(program_url)
 
-# data = element.get_attribute('data-blabla')
-# print(element.text)
+## gets pre reqs container and adds wait time for content to load
+element = WebDriverWait(browser, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "rules-wrapper"))
+)
 
-## using BeautifulSoup to parse html ##
-
+## using BeautifulSoup to parse html 
 soup = BeautifulSoup(browser.page_source, 'html.parser')
-print(soup.select(".rules-wrapper")[0].get_text())
+container = soup.select(".rules-wrapper")[0]
 
-# use ul and li to put pre req into into object, check 'complete'
+
+
+"""
+# ul denotes new requirement (complete 1 of, complete all of)
+# ul's each li denotes requirement item
+for ul
+    look into each li
+    if li has ul child
+        remove ul and keep
+    log li.text
+
+    repeat for ul
+"""
+
+def top_ul(tag):
+    return len(tag.find_parents("ul")) == 0 and tag.name == "ul"
+
+def get_requirements(ul):
+    requirements = []
+    
+    # all_top_ul = ul.find_all(top_ul)
+    # print(len(ul.find_parents("ul")) == 0)
+
+    for li in ul.find_all(["li", "div"], recursive=False):
+        nested_ul = li.find("ul")
+        if (nested_ul):
+            li_title = copy.copy(li) # title (complete all of the following, complete all of, compete 1 of)
+            li_title.find("ul").decompose()
+
+            nested_req = {
+                li_title.get_text(): get_requirements(nested_ul)
+            }
+            requirements.append(nested_req)
+        else:
+            requirements.append(li.get_text())
+
+    return requirements
+
+
+
+all_ul = container.find_all(top_ul) # root ul of all years from program
+for ul in all_ul:
+    requirements = get_requirements(ul) 
+    print( json.dumps(requirements,indent=2) )
 
 
 browser.quit()
